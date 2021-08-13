34245c6086b37e6df67303bbcbf3f77b1762f7ec
Jian He <He.Jian@ufh.com.cn>
2021-08-13 17:25:35 +0800

    增加C#版快速排序优化算法

--- Code/Solution_Algorithms/QuickSortOptimize.CSharp/Program.cs
+++ Code/Solution_Algorithms/QuickSortOptimize.CSharp/Program.cs
@@ -8,34 +8,46 @@ namespace QuickSortOptimize.CSharp
     /// </summary>
     class Program
     {
+        private static int n = 10;
+        private static int max_N = 1000000;
+        private static int[] arr1 = new int[max_N + 5];
+        private static int[] arr2 = new int[max_N + 5];
+
         static void Main(string[] args)
         {
-            IList<int> arr = new List<int> { 8, 3, 10, 2, 7, 6, 9, 12 };
-            int n = arr.Count - 1;
+            Console.WriteLine($"测试次数：{n}\n每轮测试数据量：{max_N}");
 
-            QuickSortOld(arr, 0, n);
-
-            Console.WriteLine("\n排序结果：");
-            for (int i = 0; i <= arr.Count - 1; i++)
+            for (int i = 1; i <= n; i++)
             {
-                Console.Write(arr[i] + " ");
+                Test_One(i);
             }
-            Console.ReadLine();
-
-
-            arr = new List<int> { 8, 3, 10, 2, 7, 6, 9, 12 };
-
-            QuickSortNew(arr, 0, n);
-
-            Console.WriteLine("\n排序结果：");
-            for (int i = 0; i <= arr.Count - 1; i++)
-            {
-                Console.Write(arr[i] + " ");
-            }
-            Console.ReadLine();
         }
 
-        static void QuickSortOld(IList<int> arr, int l, int r)
+        static void Test_One(int t)
+        {
+            var radom = new Random();
+
+            for (int i = 0; i < max_N; i++)
+            {
+                arr1[i] = radom.Next();
+                //a1[i] = i;
+                arr2[i] = arr1[i];
+            }
+
+            DateTime begin1 = DateTime.Now;
+            Quick_Sort_Old(arr1, 0, max_N - 1);
+            DateTime end1 = DateTime.Now;
+            double time1 = (end1 - begin1).TotalMilliseconds;
+
+            DateTime begin2 = DateTime.Now;
+            Quick_Sort_New(arr1, 0, max_N - 1);
+            DateTime end2 = DateTime.Now;
+            double time2 = (end2 - begin2).TotalMilliseconds;
+
+            Console.WriteLine($"第 {t} 轮测试，Quick_Sort_Old({time1}ms)，Quick_Sort_New({time2}ms)");
+        }
+
+        static void Quick_Sort_Old(IList<int> arr, int l, int r)
         {
             if (l >= r) return;
             int x = l, y = r, z = arr[l];
@@ -47,21 +59,22 @@ namespace QuickSortOptimize.CSharp
                 if (x < y) arr[y--] = arr[x];
             }
             arr[x] = z;
-            QuickSortOld(arr, l, x - 1);
-            QuickSortOld(arr, x + 1, r);
+            Quick_Sort_Old(arr, l, x - 1);
+            Quick_Sort_Old(arr, x + 1, r);
         }
 
-        static void QuickSortNew(IList<int> arr, int l, int r)
+        static void Quick_Sort_New(IList<int> arr, int l, int r)
         {
             while (l < r)
             {
-                int x = l, y = r, z = SelectValue(arr[l], arr[r], arr[(l + r) >> 1]);
+                int x = l, y = r, z = SelectValue(arr[l], arr[r], arr[(l + r) >> 1]); // 基准值选取优化
                 do
                 {
                     while (arr[x] < z) ++x;
                     while (arr[y] > z) --y;
                     if (x <= y)
                     {
+                        // partition 操作优化
                         arr[x] = arr[x] ^ arr[y];
                         arr[y] = arr[x] ^ arr[y];
                         arr[x] = arr[x] ^ arr[y];
@@ -69,8 +82,8 @@ namespace QuickSortOptimize.CSharp
                         y--;
                     }
                 } while (x <= y);
-                QuickSortNew(arr, x, r);
-                r = y;
+                Quick_Sort_New(arr, x, r); // 右侧正常调用递归函数 
+                r = y; // 用本层处理左侧的排序（单边递归优化）
             }
         }
 
